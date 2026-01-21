7718cb881b1f9dcc3b2d73fb2c91e60a95dd19c1
youssefkarfouh <youssefkarfouh7@gmail.com>
2026-01-21 21:18:47 +0100

    fix: update socketClient to use VITE_SOCKET_URL for dynamic socket connection

--- frontend/src/global/socketClient.ts
+++ frontend/src/global/socketClient.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const socketClient: Socket = io("http://localhost:4000", {
+const socketClient: Socket = io(import.meta.env.VITE_SOCKET_URL, {
   withCredentials: true,
 });
 
